911f01131909ea19ee09093e86ba13bee339699f
Sean E. <sembornas@mymail.mapua.edu.ph>
2025-02-05 13:42:32 +0800

    Removed API connections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { CharactersComponent } from './characters/characters.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: 'characters', component: CharactersComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent}
 ];
--- src/app/model/character.ts
+++
@@ -1,7 +0,0 @@
-export class Character {
-    id: number = 0
-    name: String = ''
-    age: number = 0
-    imgPath: String = ''
-    description: String = ''
-}
